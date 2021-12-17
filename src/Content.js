8018453b2a01fcaf76fd36e0948a134ebde9bf39
Nguyen Van Sam <53646493+finn-01@users.noreply.github.com>
2021-12-17 14:41:16 +0700

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const tabs = ["posts", "comments", "albums"];
+
+function Content() {
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+      });
+  }, [type]);
+
+  return (
+    <div>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          onClick={() => setType(tab)}
+          style={type === tab ? { color: "#fff", backgroundColor: "#333" } : {}}
+        >
+          {tab}
+        </button>
+      ))}
+
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title || post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Content;
